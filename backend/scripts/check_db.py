915c39df34dfc78635a156366c729ad988c77193
Rose Lin <lx.rose.lin@gmail.com>
2026-08-01 05:25:33 +0800

    ruff fix

--- backend/scripts/check_db.py
+++ backend/scripts/check_db.py
@@ -10,9 +10,7 @@ pool.open()
 # connection pool is like a cache of db connections
 # context manager - auto close the connection after code ends this cnotext
 # allow capture release (cheaper than open/close)
-with pool.connection() as conn:
-    # executes query
-    with conn.cursor() as cur:
+with pool.connection() as conn, conn.cursor() as cur:
         cur.execute("SELECT 1 AS ok")
         print(cur.fetchone())
 
