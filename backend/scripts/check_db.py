8e4597d081592dbf2fe51b0abd4c970ed4934dfa
Rose Lin <lx.rose.lin@gmail.com>
2026-08-01 05:09:27 +0800

    added db connection

---
+++ backend/scripts/check_db.py
@@ -0,0 +1,18 @@
+# scripts/check_db.py
+from dotenv import load_dotenv
+load_dotenv()
+
+from app.db import pool
+
+pool.open()
+
+# connection pool is like a cache of db connections
+# context manager - auto close the connection after code ends this cnotext
+# allow capture release (cheaper than open/close)
+with pool.connection() as conn:
+    # executes query
+    with conn.cursor() as cur:
+        cur.execute("SELECT 1 AS ok")
+        print(cur.fetchone())
+
+pool.close()
